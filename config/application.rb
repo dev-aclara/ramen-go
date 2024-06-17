0335c2eca8f9617ffefb6ec35dde2b79a7eeec31
Ana Clara Mansano <anamansano@jera.com.br>
2024-06-17 00:39:45 -0300

    [UPDATE] files

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module RamenGo
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://tech.redventures.com.br'  # Substitua pelo seu domínio real ou '*' para permitir de qualquer lugar
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
